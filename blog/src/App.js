f3ea0ec550e58c2f322b00da7a13b5729bd1f4fd
nao.sakano <nao.sakano@hottolink.co.jp>
2018-04-02 07:53:51 +0900

    small change

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Button from './components/Button'
 
 class App extends Component {
   render() {
+    const reloadFunction = () => {
+      window.location.reload();
+    }
+  
     return (
       <div className="App">
         <header className="App-header">
@@ -11,8 +16,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React with PWA</h1>
         </header>
         <p className="App-intro">
-          ver.3.1.2
+          ver.3.1.4
         </p>
+        <input type="button" value="このページを再読込します" onClick={reloadFunction} />
       </div>
     );
   }
---
+++ blog/src/components/Button.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import 'antd/dist/antd.css';
+import '../index.css';
+import { Button } from 'antd';
+import React, { Component } from 'react';
+
+
+class Button extends Component {
+  render  = () => {
+    retrun (
+      <div>
+        <Button>Default</Button>
+      </div>
+    )
+  }
+}
+
+export default Button
